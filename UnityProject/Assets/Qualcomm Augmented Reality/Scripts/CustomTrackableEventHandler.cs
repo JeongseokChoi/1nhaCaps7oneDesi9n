eb645b422bb2ce66787c4219ea0a5a793734ddcb
Jeongseok Choi <jeongseokchoi.korea@gmail.com>
2014-10-27 20:45:27 +0900

    20141027

--- UnityProject/Assets/Qualcomm Augmented Reality/Scripts/CustomTrackableEventHandler.cs
+++ UnityProject/Assets/Qualcomm Augmented Reality/Scripts/CustomTrackableEventHandler.cs
@@ -12,6 +12,15 @@ using UnityEngine;
 public class CustomTrackableEventHandler : MonoBehaviour,
                                             ITrackableEventHandler
 {
+	bool firstDetected = false;
+
+	int i=0, j, random_result;
+	int[] temp = new int[10]; // 
+	bool bool_tmp = false;
+	
+	public GameObject[] menu_object = new GameObject[10];
+
+
     #region PRIVATE_MEMBER_VARIABLES
  
     private TrackableBehaviour mTrackableBehaviour;
@@ -28,6 +37,43 @@ public class CustomTrackableEventHandler : MonoBehaviour,
         {
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
         }
+
+		
+		// MENU_NUMBER random setting
+		for(i=0; i<10; i++){
+			bool_tmp = false;
+			while( bool_tmp != true ){
+				
+				random_result = Random.Range (0,10);
+				bool_tmp = true;
+				for( j=0; j<i; j++){
+					if( random_result == temp[j] ){
+						bool_tmp = false;
+						break;
+					}
+				}
+			}
+			
+			temp[i] = random_result;
+			
+		}
+
+		for (i=0; i<10; i++) {	
+			float temp_X = menu_object[i].transform.position.x;
+			float temp_Y = menu_object[i].transform.position.y;
+			float temp_Z = menu_object[i].transform.position.z;
+
+			if( temp[i] == 1 || temp[i] == 4 || temp[i]== 7 ) menu_object[i].transform.position = new Vector3(-180.0f, temp_Y, temp_Z);
+			else if( temp[i] == 2 || temp[i] ==  5 || temp[i] == 8 || temp[i] == 0 ) menu_object[i].transform.position = new Vector3(0.0f, temp_Y, temp_Z);
+			else if( temp[i] == 3 || temp[i] ==  6 || temp[i] == 9 ) menu_object[i].transform.position = new Vector3(180f, temp_Y, temp_Z);
+
+			temp_X = menu_object[i].transform.position.x;
+			
+			if( temp[i] == 1 || temp[i] == 2 || temp[i] == 3 ) menu_object[i].transform.position = new Vector3(temp_X, temp_Y, 360.0f);
+			else if( temp[i] == 4 || temp[i] == 5 || temp[i] == 6 ) menu_object[i].transform.position = new Vector3(temp_X, temp_Y, 180.0f);
+			else if( temp[i] == 7 || temp[i] == 8 || temp[i] == 9 ) menu_object[i].transform.position = new Vector3(temp_X, temp_Y, 0.0f);
+			else if( temp[i] == 0 ) menu_object[i].transform.position = new Vector3(temp_X, temp_Y, -180.0f);
+		}
     }
 
     #endregion // UNTIY_MONOBEHAVIOUR_METHODS
@@ -51,7 +97,11 @@ public class CustomTrackableEventHandler : MonoBehaviour,
         }
         else
         {
-            OnTrackingLost();
+			if (firstDetected == false)
+			{
+				OnTrackingLost();
+				firstDetected = true;
+			}
         }
     }
 
@@ -84,7 +134,7 @@ public class CustomTrackableEventHandler : MonoBehaviour,
 
 
     private void OnTrackingLost()
-    {/*
+    {
         Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
         Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
 
@@ -101,7 +151,6 @@ public class CustomTrackableEventHandler : MonoBehaviour,
         }
 
         Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
-    */
 	}
 
     #endregion // PRIVATE_METHODS
